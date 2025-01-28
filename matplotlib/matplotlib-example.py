db7f344ff743543a36523083994695640337e662
limpa95 <patrickhlim@gmail.com>
2025-01-27 20:39:26 -0600

    Fixed typos

--- matplotlib/matplotlib-example.py
+++ matplotlib/matplotlib-example.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 
 y = ["Hip-hop", "Blues", "Classical", "Jazz", "Disco", "Reggae", "Country", "Pop", "Metal", "Rock"]
 w = [0, 0, 5, 10, 10, 20, 20, 50, 60, 80]
-c = ["blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue",]
+c = ["blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue"]
 
-plt.barh(y, w, 0.5, color = c)
+plt.barh(y, w, 0.5, color=c)
 plt.xlabel("Accuracy (%)")
 plt.ylabel("Genre")
 plt.title("Music Genre Classification")
