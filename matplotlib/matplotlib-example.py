7cfb06a847cc869cf00e8630e25339ebbfd746b4
limpa95 <patrickhlim@gmail.com>
2025-01-30 16:22:42 -0600

    Changed accuracy metrics to read from txt files for prediction probabilities and labels.

--- matplotlib/matplotlib-example.py
+++ matplotlib/matplotlib-example.py
@@ -1,7 +1,23 @@
 import matplotlib.pyplot as plt
 
-y = ["Hip-hop", "Blues", "Classical", "Jazz", "Disco", "Reggae", "Country", "Pop", "Metal", "Rock"]
-w = [0, 0, 5, 10, 10, 20, 20, 50, 60, 80]
+
+def read_output(name):
+    with open(name, 'r') as file:
+        string = file.read()
+        string = string.strip()
+        output_list = string.split(',')
+        if name == "prediction.txt":
+            output_list = [int(x) for x in output_list]
+    return output_list
+
+
+prediction_list = read_output("prediction.txt")
+labels_list = read_output("labels.txt")
+tuples_list = list(zip(labels_list, prediction_list))
+sorted_tuples_list = sorted(tuples_list, key=lambda x: x[1])
+
+y = [x[0] for x in sorted_tuples_list]
+w = [x[1] for x in sorted_tuples_list]
 c = ["blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue"]
 
 plt.barh(y, w, 0.5, color=c)
