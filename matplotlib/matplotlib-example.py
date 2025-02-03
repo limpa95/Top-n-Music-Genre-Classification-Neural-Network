49233f71ecaf1fb9aedd48eec7e16c6940d257f9
limpa95 <patrickhlim@gmail.com>
2025-02-03 00:37:16 -0600

    Updated accuracy metrics to read json file dictionary.

--- matplotlib/matplotlib-example.py
+++ matplotlib/matplotlib-example.py
@@ -1,19 +1,15 @@
 import matplotlib.pyplot as plt
+import json
 
 
 def read_output(name):
     with open(name, 'r') as file:
-        string = file.read()
-        string = string.strip()
-        output_list = string.split(',')
-        if name == "prediction.txt":
-            output_list = [int(x) for x in output_list]
-    return output_list
+        json_dict = json.load(file)
+    return json_dict
 
 
-prediction_list = read_output("prediction.txt")
-labels_list = read_output("labels.txt")
-tuples_list = list(zip(labels_list, prediction_list))
+output_dict = read_output("output.json")
+tuples_list = list(output_dict.items())
 sorted_tuples_list = sorted(tuples_list, key=lambda x: x[1])
 
 y = [x[0] for x in sorted_tuples_list]
